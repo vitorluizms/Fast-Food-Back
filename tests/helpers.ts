68d24551d16e494100528966348a74a374d17b6b
vitorluizms <vitor.luiz.eer@gmail.com>
2023-11-28 00:37:59 -0300

    feat: updating tables at database

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,7 +1,12 @@
 import prisma from '@/database/database';
 
 export async function cleanDb() {
-  const array = [1, 2, 3, 4, 5];
+  const array = [
+    {
+      productId: 1,
+      toppings: [1, 2, 3],
+    },
+  ];
   prisma.order.create({
     data: {
       amountPay: 1000,
@@ -9,9 +14,10 @@ export async function cleanDb() {
       ProductAndOrder: {
         createMany: {
           data: array.map(element => ({
-            productId: element,
+            productId: element.productId,
             orderId: 1,
             quantity: 1,
+            toppings: 'asdas',
           })),
         },
       },
